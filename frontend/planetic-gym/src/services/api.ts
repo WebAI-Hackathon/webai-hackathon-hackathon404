6d96573d56ad02eefc504e5016754598def1969f
Naguleswaran, Mithusan <mithusan.naguleswaran@stud.tu-darmstadt.de>
2025-07-25 10:24:12 +0200

    base url

--- frontend/planetic-gym/src/services/api.ts
+++ frontend/planetic-gym/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://webai-hackathon-hackathon404.onrender.com";
 
 // Types basierend auf Backend Schemas
 export interface Exercise {
