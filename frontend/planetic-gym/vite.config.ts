9d92c0dbbf0b7bfd8c1f8c74e94bf3b5f42a4ef6
Naguleswaran, Mithusan <mithusan.naguleswaran@stud.tu-darmstadt.de>
2025-07-25 13:23:43 +0200

    last build

--- frontend/planetic-gym/vite.config.ts
+++ frontend/planetic-gym/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/webai-hackathon-hackathon404",
+  base: "webai-hackathon-hackathon404",
   build: {
     outDir: "../../docs",
     emptyOutDir: true,
