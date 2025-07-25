4f458b6ba35a5d7a18434af7da02797687eb2731
Naguleswaran, Mithusan <mithusan.naguleswaran@stud.tu-darmstadt.de>
2025-07-25 10:30:14 +0200

    cors *

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],  # Für Vite Dev Server
+    allow_origins=["*"],  # Für Vite Dev Server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
